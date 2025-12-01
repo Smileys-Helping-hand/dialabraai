eaf569aeea9580f2f37190a2b15942e0d38c7b14
backupe9 <backupe9@gmail.com>
2025-12-01 13:24:09 +0000

    feat: add background animation and visual effects

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,53 +1,59 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
+  # To learn more about how to use Nix to configure your environment
+  # see: https://developers.google.com/idx/guides/customize-idx-env
+
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
+    pkgs.nodejs_20
+    pkgs.firebase-tools
   ];
+
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    NEXT_PUBLIC_FIREBASE_API_KEY = "AIzaSyDL7pjQ2QKOsiMASKa-Lta2BYnUOnnOJtk";
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN = "dialabraai-78714.firebaseapp.com";
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID = "dialabraai-78714";
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET = "dialabraai-78714.appspot.com";
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID = "102296308816";
+    NEXT_PUBLIC_FIREBASE_APP_ID = "1:102296308816:web:2bd977d065cc40feb81555";
+    GEMINI_API_KEY = "AIzaSyAYXhnUhF7zBnG_bQGq-ysjgOoGm8tzSJM";
+  };
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
       "google.gemini-cli-vscode-ide-companion"
     ];
-    # Enable previews
+
+    # Enable previews and define a preview for your web application
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          # The command to start your development server
+          command = ["sh" "-c" "cd dialabraai && npm run dev -- --port $PORT"];
+          manager = "web";
+        };
       };
     };
+
     # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ ".idx/dev.nix" "README.md" ];
+        # Install project dependencies
+        npm-install = "cd dialabraai && npm install";
+        # Open these files by default
+        default.openFiles = [ ".idx/dev.nix" "README.md" "dialabraai/lib/firebase.js" ];
       };
+
       # Runs when the workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        # The dev server is started as part of the preview configuration,
+        # so we don't need to start it here.
       };
     };
   };
